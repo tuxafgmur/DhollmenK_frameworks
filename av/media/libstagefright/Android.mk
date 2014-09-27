9d2815b69b57c78c4c512e7918e3f1ddd5fcf642
tuxafgmur <usuario@localhost.localdomain>
2014-09-27 21:39:49 +0200

    Initial release
    Copy of  https://github.com/SlimRoms/frameworks_av - kk4.4 - 2014.09.27

---
+++ av/media/libstagefright/Android.mk
@@ -0,0 +1,200 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+include frameworks/av/media/libstagefright/codecs/common/Config.mk
+
+ifeq ($(BOARD_HTC_3D_SUPPORT),true)
+   LOCAL_CFLAGS += -DHTC_3D_SUPPORT
+endif
+
+LOCAL_SRC_FILES:=                         \
+        ACodec.cpp                        \
+        AACExtractor.cpp                  \
+        AACWriter.cpp                     \
+        AMRExtractor.cpp                  \
+        AMRWriter.cpp                     \
+        AudioPlayer.cpp                   \
+        AudioSource.cpp                   \
+        AwesomePlayer.cpp                 \
+        CameraSource.cpp                  \
+        CameraSourceTimeLapse.cpp         \
+        DataSource.cpp                    \
+        DRMExtractor.cpp                  \
+        ESDS.cpp                          \
+        FileSource.cpp                    \
+        FLACExtractor.cpp                 \
+        HTTPBase.cpp                      \
+        JPEGSource.cpp                    \
+        MP3Extractor.cpp                  \
+        MPEG2TSWriter.cpp                 \
+        MPEG4Extractor.cpp                \
+        MPEG4Writer.cpp                   \
+        MediaAdapter.cpp                  \
+        MediaBuffer.cpp                   \
+        MediaBufferGroup.cpp              \
+        MediaCodec.cpp                    \
+        MediaCodecList.cpp                \
+        MediaDefs.cpp                     \
+        MediaExtractor.cpp                \
+        MediaMuxer.cpp                    \
+        MediaSource.cpp                   \
+        MetaData.cpp                      \
+        NuCachedSource2.cpp               \
+        NuMediaExtractor.cpp              \
+        OMXClient.cpp                     \
+        OMXCodec.cpp                      \
+        OggExtractor.cpp                  \
+        SampleIterator.cpp                \
+        SampleTable.cpp                   \
+        SkipCutBuffer.cpp                 \
+        StagefrightMediaScanner.cpp       \
+        StagefrightMetadataRetriever.cpp  \
+        SurfaceMediaSource.cpp            \
+        ThrottledSource.cpp               \
+        TimeSource.cpp                    \
+        TimedEventQueue.cpp               \
+        Utils.cpp                         \
+        VBRISeeker.cpp                    \
+        WAVExtractor.cpp                  \
+        WAVEWriter.cpp                    \
+        WVMExtractor.cpp                  \
+        XINGSeeker.cpp                    \
+        avc_utils.cpp                     \
+        mp4/FragmentedMP4Parser.cpp       \
+        mp4/TrackFragment.cpp             \
+
+LOCAL_C_INCLUDES:= \
+        $(TOP)/frameworks/av/include/media/stagefright/timedtext \
+        $(TOP)/frameworks/native/include/media/hardware \
+        $(TOP)/frameworks/native/include/media/openmax \
+        $(TOP)/frameworks/native/services/connectivitymanager \
+        $(TOP)/external/flac/include \
+        $(TOP)/external/tremolo \
+        $(TOP)/external/openssl/include \
+
+ifneq ($(filter caf bfam,$(TARGET_QCOM_AUDIO_VARIANT)),)
+    ifeq ($(BOARD_USES_LEGACY_ALSA_AUDIO),true)
+        ifeq ($(call is-chipset-in-board-platform,msm8960),true)
+            LOCAL_SRC_FILES += LPAPlayerALSA.cpp TunnelPlayer.cpp
+            LOCAL_CFLAGS += -DUSE_TUNNEL_MODE -DUSE_LPA_MODE
+            LOCAL_CFLAGS += -DTUNNEL_MODE_SUPPORTS_AMRWB
+        endif
+        ifeq ($(call is-chipset-in-board-platform,msm8974),true)
+            # If you are using legacy mode on 8974, you will not
+            # go to space today. Also, it probably is broken.
+            LOCAL_SRC_FILES += LPAPlayerALSA.cpp TunnelPlayer.cpp
+            LOCAL_CFLAGS += -DUSE_TUNNEL_MODE -DUSE_LPA_MODE
+        endif
+        ifeq ($(call is-chipset-in-board-platform,msm8660),true)
+            LOCAL_SRC_FILES += LPAPlayer.cpp
+            LOCAL_CFLAGS += -DLEGACY_LPA -DUSE_LPA_MODE
+        endif
+        ifeq ($(NO_TUNNEL_MODE_FOR_MULTICHANNEL),true)
+            LOCAL_CFLAGS += -DNO_TUNNEL_MODE_FOR_MULTICHANNEL
+        endif
+    endif
+endif
+
+ifneq ($(TARGET_QCOM_MEDIA_VARIANT),)
+LOCAL_C_INCLUDES += \
+        $(TOP)/hardware/qcom/media-$(TARGET_QCOM_MEDIA_VARIANT)/mm-core/inc
+else
+LOCAL_C_INCLUDES += \
+        $(TOP)/hardware/qcom/media/mm-core/inc
+endif
+
+LOCAL_SHARED_LIBRARIES := \
+        libbinder \
+        libcamera_client \
+        libconnectivitymanager \
+        libcutils \
+        libdl \
+        libdrmframework \
+        libexpat \
+        libgui \
+        libicui18n \
+        libicuuc \
+        liblog \
+        libmedia \
+        libsonivox \
+        libssl \
+        libstagefright_omx \
+        libstagefright_yuv \
+        libsync \
+        libui \
+        libutils \
+        libvorbisidec \
+        libz \
+        libpowermanager
+
+LOCAL_STATIC_LIBRARIES := \
+        libstagefright_color_conversion \
+        libstagefright_mp3dec \
+        libstagefright_aacenc \
+        libstagefright_matroska \
+        libstagefright_timedtext \
+        libvpx \
+        libwebm \
+        libstagefright_mpeg2ts \
+        libstagefright_id3 \
+        libFLAC \
+        libmedia_helper
+
+
+LOCAL_SRC_FILES += ExtendedCodec.cpp ExtendedExtractor.cpp ExtendedUtils.cpp
+
+ifeq ($(TARGET_ENABLE_QC_AV_ENHANCEMENTS),true)
+       LOCAL_CFLAGS     += -DENABLE_AV_ENHANCEMENTS
+       LOCAL_SRC_FILES  += ExtendedMediaDefs.cpp ExtendedWriter.cpp
+
+       ifneq ($(TARGET_QCOM_MEDIA_VARIANT),)
+           LOCAL_C_INCLUDES += \
+               $(TOP)/hardware/qcom/media-$(TARGET_QCOM_MEDIA_VARIANT)/mm-core/inc
+       else
+           LOCAL_C_INCLUDES += \
+               $(TOP)/hardware/qcom/media/mm-core/inc
+       endif
+endif #TARGET_ENABLE_AV_ENHANCEMENTS
+
+LOCAL_SRC_FILES += \
+        chromium_http_stub.cpp
+LOCAL_CPPFLAGS += -DCHROMIUM_AVAILABLE=1
+
+LOCAL_SHARED_LIBRARIES += libstlport
+include external/stlport/libstlport.mk
+
+LOCAL_SHARED_LIBRARIES += \
+        libstagefright_enc_common \
+        libstagefright_avc_common \
+        libstagefright_foundation \
+        libdl
+
+LOCAL_CFLAGS += -Wno-multichar
+
+ifeq ($(BOARD_USE_SAMSUNG_COLORFORMAT), true)
+LOCAL_CFLAGS += -DUSE_SAMSUNG_COLORFORMAT
+
+# Include native color format header path
+LOCAL_C_INCLUDES += \
+	$(TOP)/hardware/samsung/exynos4/hal/include \
+	$(TOP)/hardware/samsung/exynos4/include
+
+endif
+
+ifeq ($(BOARD_USE_TI_DUCATI_H264_PROFILE), true)
+LOCAL_CFLAGS += -DUSE_TI_DUCATI_H264_PROFILE
+endif
+
+ifdef DOLBY_UDC
+  LOCAL_CFLAGS += -DDOLBY_UDC
+endif #DOLBY_UDC
+ifdef DOLBY_UDC_MULTICHANNEL
+  LOCAL_CFLAGS += -DDOLBY_UDC_MULTICHANNEL
+endif #DOLBY_UDC_MULTICHANNEL
+LOCAL_MODULE:= libstagefright
+
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_SHARED_LIBRARY)
+
+include $(call all-makefiles-under,$(LOCAL_PATH))
